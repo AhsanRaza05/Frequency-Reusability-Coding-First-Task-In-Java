635a7f74993e3c2f696bbfd0428e0c26405d91a6
Ahsan Raza <ahsanrazamemon2000@gmail.com>
2022-08-02 11:38:38 +0500

    Added Comments

--- Main.java
+++ Main.java
@@ -10,8 +10,6 @@ public class Main {
      String x = new Scanner(System.in).nextLine();
 	 
 	 
-	 ////System.out.println("%"+x+"%");
-	 
 	 // Removing all white spaces
 	 x = x.replace(" ","");
 	 
@@ -24,45 +22,34 @@ public class Main {
 	 // Removing the Commas at Ending
 	 x = x.replaceAll(",$","");
 	 
-	 
-	 ////System.out.println("%"+x+"%");
-	 
+	 // Split the input string (lsit of numbers) to Arrays of string, to sperate the values 
 	 String input[] = x.split(",");
-     ////System.out.println("N: "+Arrays.toString(input));
-     //System.out.println(Arrays.toString(input));
-     
-     //Trim the values of String
-     //Arrays.parallelSetAll(input, (i) -> input[i].trim());
-     ///Arrays.stream(array).map(String::trim).toArray(unused -> array);
-     
-	 // Remove the Leading and ending of commas
-     //Arrays.parallelSetAll(input, (i) -> input[i].replace(",",""));
-	 
-     //System.out.println(Arrays.toString(input));
      
      // Cast String array to array of Integers
      int N[] = Arrays.stream(input).mapToInt(Integer::parseInt).toArray();
-     //N = Stream.of(input).mapToInt(Integer::parseInt).toArray();
-                  
-      ////System.out.println("N: "+Arrays.toString(N));
+    
 
+      // LOOP to iterate all elements of List N
      l1:for(int a = 0; a < N.length ;a++){
 
          System.out.println("For N:"+N[a]);
 
          int sqrt = (int) Math.sqrt(N[a]) ;
          
+         // To check the case # 1 when value of J = 0 
          if(sqrt * sqrt == N[a]){
              System.out.println(String.format("\t RESULT: i = %d, j = %d <>",sqrt,0));
          }
          
          else{
              
+            // Loop for values of i
              l2:for(int i = 1; i <= sqrt; i++){
 
+                // Loop for values of j
              l3:for(int j = 1; j <= (int) Math.sqrt(sqrt); j++){
 
-                //System.out.println("\t"+i+" "+j);
+                // Check the combination of i & j which staisfies the formula
                 if (i * i + i * j + j * j == N[a]){
 
                     System.out.println(String.format("\t RESULT: i = %d, j = %d",i,j));
@@ -71,6 +58,7 @@ public class Main {
              }
 
          } 
+         
         System.out.println("\t i & j does not exist!");
              
          }
